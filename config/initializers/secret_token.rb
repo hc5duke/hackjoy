098c763cb49b1b8ec456a260b8811110221cd47b
hc5duke <hc5duke@gmail.com>
2012-05-28 23:07:05 -0700

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hackjoy::Application.config.secret_token = '38a566503d7a730d5e10043878ef1c877129895ad0f33ba331682a682d84f0229b12e32d16a3a4994234eb78407497f4b7917b9675647bc24c800ae2e7ef2fa4'
